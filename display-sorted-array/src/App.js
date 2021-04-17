5a9e317a38cd6628230e6670c7176f6f19e81074
shruti <shrutipatil1121@gmail.com>
2021-04-17 04:20:41 -0400

    sort the array from the frequency of elements

--- display-sorted-array/src/App.js
+++ display-sorted-array/src/App.js
@@ -5,7 +5,45 @@ import './App.css';
 function App() {
   const element = useRef();
   const [anArray,setAnArray]  = useState([]);
+  const [sortedArray,setSortedArray]  = useState([]);
  
+  useEffect(()=>{
+    let newArr = sortAnArray(anArray);
+    setSortedArray(newArr);
+
+  },[anArray]);
+
+   const sortAnArray = (array) => {
+
+      let mapOfarra = {};
+      let sortedWithFreq = [];
+      let tempArr = [];
+  
+      array.forEach(function(value) { 
+          if ( value in mapOfarra )
+          mapOfarra[value] = mapOfarra[value] + 1;
+          else
+          mapOfarra[value] = 1;
+      });
+      
+  
+      for(var key in mapOfarra){
+        sortedWithFreq.push([key, mapOfarra[key]])
+      }
+  
+      sortedWithFreq.sort(function(a, b){
+          return b[1] - a[1]
+      })
+  
+      
+      sortedWithFreq.forEach(function(obj){
+          for(var i=0; i < obj[1]; i++){
+            tempArr.push(obj[0]);
+          }
+      })
+      return tempArr;
+      
+  }
 
   const addElementsToAnArr =  () => {
      setAnArray(array => [...array,element.current.value]);
